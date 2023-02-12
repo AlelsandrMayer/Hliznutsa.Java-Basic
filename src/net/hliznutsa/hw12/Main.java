4d5ed63807a8fd5cb600442941946e0548cd176c
AleksandrMayer <Aleksandr_gliznutsa@ukr.net>
2023-02-12 22:52:53 +0200

    re

--- src/net/hliznutsa/hw12/Main.java
+++ src/net/hliznutsa/hw12/Main.java
@@ -4,8 +4,11 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        Person out = new Person();
-        String firstPers = out.personInfo("Will", "Smith", "New York", "5435246234634257");
-        System.out.println(firstPers);
+        Person firstPerson = new Person("Steven", "Sigle", "Elenovka", "+30983412405");
+        System.out.println(firstPerson.personInfo());
+        Person secondPerson = new Person("Donald", "Tramp", "Zhmerinka", "+380956324093");
+        System.out.println(secondPerson.personInfo());
+        Person thirdPerson = new Person("Adolph", "Hitler", "Hell", "666");
+        System.out.println(thirdPerson.personInfo());
     }
 }
--- src/net/hliznutsa/hw12/Person.java
+++ src/net/hliznutsa/hw12/Person.java
@@ -6,19 +6,14 @@ public class Person {
     public String city;
     public String phoneNum;
 
-    public String personInfo(String name, String surname, String city, String phoneNum) {
-        setValues(name, surname, city, phoneNum);
-        return getValues();
+    public String personInfo() {
+        return "Позвонить гражданину " + this.name + " " + this.surname + ", c города " + this.city + ", можно по номеру " + this.phoneNum;
     }
 
-    public void setValues(String name, String surname, String city, String phoneNum) {
+    public Person(String name, String surname, String city, String phoneNum) {
         this.name = name;
         this.surname = surname;
         this.city = city;
         this.phoneNum = phoneNum;
-    }
-
-    public String getValues() {
-        return "Позвонить гражданину " + this.name + " " + this.surname + ", c города " + this.city + ", можно по номеру " + this.phoneNum;
     }
 }
