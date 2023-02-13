b99d1a8760f788c68b4e226bcea11d8e512accfe
AleksandrMayer <Aleksandr_gliznutsa@ukr.net>
2023-02-13 22:57:02 +0200

    HW13.RE

--- src/net/hliznutsa/hw13/Burger.java
+++ src/net/hliznutsa/hw13/Burger.java
@@ -2,14 +2,14 @@ package net.hliznutsa.hw13;
 
 public class Burger {
     private String bun;
-    private String[] meat = new String[2];
+    private String[] meat;
     private String cheese;
     private String greens;
     private String mayonnaise;
 
     public Burger(String bun, String meat, String cheese, String greens, String mayonnaise) {
         this.bun = bun;
-        this.meat[0] = meat;
+        this.meat = new String[]{meat};
         this.cheese = cheese;
         this.greens = greens;
         this.mayonnaise = mayonnaise;
@@ -18,7 +18,7 @@ public class Burger {
 
     public Burger(String bun, String meat, String cheese, String greens) {
         this.bun = bun;
-        this.meat[0] = meat;
+        this.meat = new String[]{meat};
         this.cheese = cheese;
         this.greens = greens;
         System.out.println("Диетический бургер - состав: " + this.bun + ", " + this.meat[0] + ", " + this.cheese + ", " + this.greens);
@@ -26,8 +26,7 @@ public class Burger {
 
     public Burger(String bun, String meat, String secondMeat, String cheese, String greens, String mayonnaise) {
         this.bun = bun;
-        this.meat[0] = meat;
-        this.meat[1] = secondMeat;
+        this.meat = new String[]{meat, secondMeat};
         this.cheese = cheese;
         this.greens = greens;
         this.mayonnaise = mayonnaise;
